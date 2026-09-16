dae4316eba6058535d32ca366f93048fa5e49ec6
Galaxy5487 <irbaz5487@gmail.com>
2026-09-16 22:59:10 +0530

    feat: add stats API endpoint to retrieve monthly employee statistics

--- api/stats.js
+++ api/stats.js
@@ -1,7 +1,8 @@
 import supabase from './db-client.js';
 import { computeMonthStats, LOW_SCORE_THRESHOLD } from './_scores.js';
+import { netlifyAdapter } from './_adapter.js';
 
-export default async function handler(req, res) {
+export async function mainHandler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -31,3 +32,7 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: err.message });
   }
 }
+
+export default mainHandler;
+export const handler = netlifyAdapter(mainHandler);
+
